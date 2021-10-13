01ba9ebe3f08ca702f4560ed32824fc1867b7490
Сергій Пемахов <pemakhov@kbp.radiy.com>
2021-10-13 16:18:51 +0300

    feat: add styles, fonts, logo

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes, { oneOfType } from 'prop-types';
+import './Layout.scss';
 
 const Layout = ({ children }) => {
   return (
     <>
       <header>
         <div className="centred-container">
-          Header
+          <div className="logo">
+            {'{ jokes }'}
+          </div>
         </div>
       </header>
       <main>
@@ -15,8 +18,13 @@ const Layout = ({ children }) => {
         </div>
       </main>
       <footer>
-        <div className="centred-container">
-          By Serhii Pemakhov for ONIX react-native internship 2021-2022
+        <div className="centred-container footer">
+          <div className="footer__name">
+            ONIX react-native internship 2021-2022
+          </div>
+          <div className="footer__copyright">
+            by Serhii Pemakhov
+          </div>
         </div>
       </footer>
     </>
