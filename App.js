84de289579e879db1af6024e4930b06397954853
pemakhov <serhiy.pemakhov@gmail.com>
2021-11-14 21:50:05 +0200

    refactor: styles
    drill styles into header through props

--- App.js
+++ App.js
@@ -4,27 +4,47 @@ import Header from './components/Header/Header';
 import { TITLE } from './constants/layout';
 
 const App = () => {
-  const [name, setName] = useState('')
+  const [name, setName] = useState('');
 
   return (
-  <SafeAreaView>
-    <Header title={TITLE} />
-    <View>
-      <Text>Name: {name}</Text>
-    </View>
-    <TextInput
-      onChangeText={setName}
-      value={name}
-      style={styles.textInput}
-    />
-  </SafeAreaView>
-)};
+    <SafeAreaView>
+      <Header
+        title={TITLE}
+        style={styles.header}
+      />
+      <View style={styles.content}>
+        <Text>Name: {name}</Text>
+        <TextInput
+          onChangeText={setName}
+          value={name}
+          style={styles.textInput}
+        />
+      </View>
+    </SafeAreaView>
+  )
+};
 
 const styles = StyleSheet.create({
+  header: {
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      backgroundColor: 'maroon',
+    },
+    text: {
+      fontSize: 20,
+      color: 'cornsilk',
+    },
+  },
+  content: {
+    paddingHorizontal: 2,
+    color: 'black',
+  },
   textInput: {
     borderWidth: 1,
     borderColor: 'black',
+    color: 'black',
   }
-})
+});
 
 export default App;
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import PropTypes from 'prop-types';
-import { THEME } from '../../constants/layout';
+import { View, Text, StyleSheet, ViewPropTypes } from 'react-native';
+import PropTypes, { shape } from 'prop-types';
 
-const Header = ({ title }) => (
-  <View style={styles.container}>
+const Header = ({ title, style }) => (
+  <View style={style.container}>
     <Text style={styles.text}>{title}</Text>
   </View>
 );
 
-const { color, backgroundColor } = THEME.title;
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    paddingTop: 10,
-    paddingBottom: 10,
-    backgroundColor,
-  },
-  text: {
-    fontSize: 20,
-    color,
-  }
-})
-
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  style: PropTypes.shape({
+    container: PropTypes.ViewPropTypes.style,
+    text: PropTypes.ViewPropTypes.style,
+  }).isRequired,
 };
 
 export default Header;
