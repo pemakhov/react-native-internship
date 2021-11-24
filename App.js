898c6c0a6e26c2ffad9e4deb90e87e3cd24a6f8a
Сергій Пемахов <pemakhov@kbp.radiy.com>
2021-11-24 17:50:50 +0200

    feat: add navigation
    only home screen for now

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home/Home';
 
-const App = () => <Home />;
+const Stack = createNativeStackNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
 export default App;
