a48c799f1135ee08d7dc44a12d64169c9527b468
Сергій Пемахов <pemakhov@kbp.radiy.com>
2021-12-17 15:45:08 +0200

    refactor: delete unnecessary comment

--- src/screens/SignIn/components/ValidationSchema.js
+++ src/screens/SignIn/components/ValidationSchema.js
@@ -4,7 +4,6 @@ const ValidationSchema = Yup.object().shape({
   name: Yup.string()
     .min(2, 'Too short')
     .max(20, 'Too long')
-    // .matches(/^\{Lu}/, { message: 'Should start with a capital letter' })
     .matches(/^[A-Za-z]+$/, 'Should contain only latin letters')
     .required('Required'),
 });
